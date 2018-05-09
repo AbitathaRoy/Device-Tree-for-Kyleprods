ddd9eb952ca585f15961a32d779ca8f8761309d4
AbitathaRoy <34188105+AbitathaRoy@users.noreply.github.com>
2018-05-09 15:46:11 +0530

    Add files via upload

---
+++ xosp.mk
@@ -0,0 +1,16 @@
+#BootAnimation
+XOSP_BOOTANIMATION_NAME := 480
+
+# Inherit some common xosp stuff.
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/xosp.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/kyleprods/full_kyleprods.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := kyleprods
+PRODUCT_NAME := xosp_kyleprods
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := S Duos 2
+PRODUCT_MANUFACTURER := Samsung
